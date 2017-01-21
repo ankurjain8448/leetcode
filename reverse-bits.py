331615efdf76499dd6a57c7e86e6a40cd48bc294
Ankur Jain <ankurjain8448@gmail.com>
2017-01-21 15:29:03 +0530

    reverse-bits#190

---
+++ reverse-bits.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        a = [0]*32
+        temp = n
+        i = 31
+        while n:
+            a[i] = n%2
+            n /= 2
+            i -= 1
+        p = 1
+        ans = 0
+        for i in a:
+            ans = ans + i*p
+            p = p << 1
+        return ans
