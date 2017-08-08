cd142735c81ff1ca22f1d1c6624c5ab578c7606f
Ankur Jain <ankurjain8448@gmail.com>
2017-08-08 16:56:07 +0530

    two-sum-iv-input-is-a-bst.py

--- two-sum-iv-input-is-a-bst.py
+++ two-sum-iv-input-is-a-bst.py
@@ -6,12 +6,11 @@
 #         self.right = None
 
 class Solution(object):
-    def findTarget(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: bool
-        """
+
+    def basic_approach(self, root, k):
+        '''
+            time/space : O(n)
+        '''
         lis = []
         def inorder(node, lis):
             if node:
@@ -31,4 +30,48 @@ class Solution(object):
             else:
                 s += 1
         return False
-            
+
+    def space_optimized(self, root, k):
+        '''
+            space : O(logn) , this is recursion depth if tree if tree is balanced
+            time : O(n)
+        '''
+        # some issues while submitting
+        ino = self.inorder(root)
+        reverse_ino = self.reverse_inorder(root)
+        try:
+            p1 = ino.next()
+            p2 = reverse_ino.next()
+            while True:
+                ss = p1+p2
+                if ss == k:
+                    break
+                elif ss > k :
+                    p2 = reverse_ino.next()
+                else:
+                    p1 = ino.next()
+            return True
+        except Exception as exp:
+            pass
+        return False
+
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        return self.basic_approach(root, k)
+        # return self.space_optimized(root, k)
+
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+            yield node.data
+            self.inorder(node.right)
+
+    def reverse_inorder(self, node):
+        if node:
+            self.reverse_inorder(node.right)
+            yield node.data
+            self.reverse_inorder(node.left)      
