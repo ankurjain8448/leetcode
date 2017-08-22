f5af540fb654adb0d8f51348314214cb25beb0ab
Ankur Jain <ankurjain8448@gmail.com>
2017-08-22 10:57:59 +0530

    reverse-integer.py

--- reverse-integer.py
+++ reverse-integer.py
@@ -1,8 +1,26 @@
+# class Solution(object):
+#     def reverse(self, x):
+#         """
+#         :type x: int
+#         :rtype: int
+#         """
+#         rev = int(str(abs(x))[::-1])
+#         return 0 if len(str(bin(rev))[2:]) > 31 else int(rev) if x > 0 else -int(rev)
+
 class Solution(object):
+    def get_rev(self, n):
+        temp = 0
+        while n:
+            rem = n%10
+            n /=10
+            temp = temp*10 + rem
+        if temp > 2**31:
+            temp = 0
+        return temp
+
     def reverse(self, x):
         """
         :type x: int
         :rtype: int
         """
-        rev = int(str(abs(x))[::-1])
-        return 0 if len(str(bin(rev))[2:]) > 31 else int(rev) if x > 0 else -int(rev)
+        return self.get_rev(x) if x >= 0 else -self.get_rev(-x)
