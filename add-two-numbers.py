8148043347b9a04986232a4e206be6a1d1cc6024
Ankur Jain <ankurjain8448@gmail.com>
2017-08-30 18:47:02 +0530

    add-two-numbers.py

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -5,28 +5,41 @@
 #		 self.next = None
 
 class Solution(object):
+	def add_carry_only(self, carry, list_, new_list):
+		while list_:
+			temp = carry + list_.val
+			carry = 1 if temp > 9 else 0
+			if new_list:
+				new_list.next = ListNode(temp%10)
+				new_list = new_list.next
+			else:
+				new_list = ListNode(temp%10)
+				self.head = new_list
+			list_ = list_.next
+		if carry:
+			new_list.next = ListNode(carry)
+
 	def addTwoNumbers(self, l1, l2):
 		"""
 		:type l1: ListNode
 		:type l2: ListNode
 		:rtype: ListNode
 		"""
+		new_list = None
 		carry = 0
+		self.head = None
+		ptr_l1 = None
 		while l1 and l2:
 			temp = carry + l1.val + l2.val
-			if temp > 9:
-				carry = 1
+			carry = 1 if temp > 9 else 0
+			if new_list:
+				new_list.next = ListNode(temp%10)
+				new_list = new_list.next
 			else:
-				carry = 0
-			l1.val = temp % 10
+				new_list = ListNode(temp%10)
+				self.head = new_list
 			l1 = l1.next
 			l2 = l2.next
-			print l1.val
-		while l1:
-			l1 = l1.next
 
-		while l2:
-			l1.next = ListNode(l2.val)
-			l1 = l1.next
-			l2 = l2.next
-		return l1
+		self.add_carry_only(carry, l1 if l1 else l2, new_list)
+		return self.head
