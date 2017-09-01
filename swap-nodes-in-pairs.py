9ab36a2ec7caf37ea14f5ebfde4ea96beb5a6d84
Ankur Jain <ankurjain8448@gmail.com>
2017-09-01 22:44:27 +0530

    swap-nodes-in-pairs.py

--- swap-nodes-in-pairs.py
+++ swap-nodes-in-pairs.py
@@ -1,34 +1,32 @@
 # Definition for singly-linked list.
 # class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+#	 def __init__(self, x):
+#		 self.val = x
+#		 self.next = None
 
 class Solution(object):
-    def swapPairs(self, current):
-        """
-        :type current: ListNode
-        :rtype: ListNode
-        """
-        '''
-			1->2->3->4
-			2->1->4->3
-        '''
-        if not current : return current
-        new_head = current
-        ptr = None
-        while current.next:
-        	if ptr is None:
-        		ptr = current.next
-        		new_head = ptr
-        	else:
-        		ptr.next = current
-        	next = current.next
-        	new_current = next.next
-        	# swapping
-        	next.next = current
-        	current.next = new_current
-        	current = new_current
-        	if not current:
-        		break
-        return new_head
+	def swapPairs(self, current):
+		"""
+		:type current: ListNode
+		:rtype: ListNode
+		"""
+		'''
+			0->1->2->3->4
+			0->2->1->3->4
+			0->2->1->4->3
+		'''
+		if not current or not current.next: return current
+		dummtHead = ListNode(0)
+		dummtHead.next = current
+		p = current
+		prev = dummtHead
+		while p and p.next:
+			q = p.next
+			r = q.next
+			prev.next = q
+			q.next = p
+			p.next = r
+			prev = p
+			p = r
+		return dummtHead.next
+			
