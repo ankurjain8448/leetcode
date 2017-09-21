45fa4c984a4ebf01ae779f977a83264d6a196181
Ankur Jain <ankurjain8448@gmail.com>
2017-09-21 11:41:05 +0530

    copy-list-with-random-pointer.py

--- copy-list-with-random-pointer.py
+++ copy-list-with-random-pointer.py
@@ -11,28 +11,21 @@ class Solution(object):
 		:type head: RandomListNode
 		:rtype: RandomListNode
 		"""
-		index = 0
-		map_node_index = {}
-		map_new_node_index = {}
-
 		temp = head
-
 		dummyHead = RandomListNode(00)
 		new_list = dummyHead
+		mappings = {}
 		while temp:
-			new_List.next = RandomListNode(temp.label)
-			new_List = new_List.next
-
-			map_new_node_index[index] = new_List
-			map_node_index[temp] = index
+			new_list.next = RandomListNode(temp.label)
+			mappings[temp] = new_list.next
+			new_list = new_list.next
 			temp = temp.next
-			index += 1
 
 		old_list = head
 		new_list = dummyHead.next
 		while old_list is not None:
 			if old_list.random:
-				new_list.random = map_new_node_index[map_node_index[old_list.random]]
+				new_list.random = mappings[old_list.random]
 			old_list = old_list.next
 			new_list = new_list.next
 
